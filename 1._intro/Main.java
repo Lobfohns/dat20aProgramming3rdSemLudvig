a5d6ca71aae28d89cbeba75a7be3061752639ade
Lobfohns <ludv1428@stud.kea.dk>
2021-08-26 11:46:02 +0200

    alt muligt fedt

--- 1._intro/Main.java
+++ 1._intro/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+        //Systemos systemos = new Systemos();
+        Systemos.out.print("hej");
+        Systemos.out.print(5);
+        Systemos.out.print(5.00);
     }
+
 }
---
+++ 1._intro/Systemos.java
@@ -0,0 +1,35 @@
+import java.io.PrintStream;
+
+public class Systemos {
+
+    public static Systemos out;
+
+    public static void print(String h){
+        System.out.println(h);
+    }
+    public static void print(int h){
+        System.out.println(h);
+    }
+    public static void print(double h){
+        System.out.println(h);
+    }
+    public static void print(byte h){
+        System.out.println(h);
+    }
+    public static void print(short h){
+        System.out.println(h);
+    }
+    public static void print(long h){
+        System.out.println(h);
+    }
+    public static void print(float h){
+        System.out.println(h);
+    }
+    public static void print(boolean h){
+        System.out.println(h);
+    }
+    public static void print(char h){
+        System.out.println(h);
+    }
+}
+
